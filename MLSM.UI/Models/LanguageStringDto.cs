9901e62e6a8fa7766b6e3074411d435a7a83959d
Ather <Ather@sme.gov.om>
2025-04-07 12:42:17 +0400

    Component to set Language text (Initially, only English and Arabic) Only UI, no backend implementation

---
+++ MLSM.UI/Models/LanguageStringDto.cs
@@ -0,0 +1,14 @@
+﻿namespace MLSM.UI.Models
+{
+    public class LanguageStringResponseDto
+    {
+    }
+    public class LanguageStringComponentResponseDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ButtonText { get; set; } = string.Empty;
+        public string ButtonColor { get; set; } = string.Empty;
+
+    }
+}
